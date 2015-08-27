6dbfaa7b6f5c4168f261ef3e8066546c1116680c
sluisp <sluisp@.(none)>
2015-08-27 18:48:15 +0200

    Deleted elements from source control.

--- java_examples/src/com/codice/apiexamples/api/actions/ObjectType.java
+++
@@ -1,9 +0,0 @@
-package com.codice.apiexamples.api.actions;
-
-import com.google.gson.annotations.SerializedName;
-
-public enum ObjectType {
-    @SerializedName("changeset") CHANGESET,
-    @SerializedName("label") LABEL,
-    @SerializedName("branch")BRANCH
-}
